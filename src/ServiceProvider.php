a3d0fc352bb7dd6dacddb8148c2924c9b2a151d1
Damian-portatil <damian.aguilarm@gmail.com>
2022-09-02 10:30:48 +0200

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,39 +2,53 @@
 
 namespace Daguilarm\Forms;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider as PackageProvider;
+use Illuminate\View\Compilers\BladeCompiler;
 
 final class ServiceProvider extends PackageProvider
 {
     /**
      * Bootstrap the application services.
-     *
-     * @return void
      */
     public function boot(): void
     {
-        //Resources 
-        $this->registerResources();
+        //Resources
+        $this->bootResources();
+        $this->bootBladeComponents();
     }
 
     /**
      * Register the application services.
-     *
-     * @return void
      */
     public function register(): void
     {
-        // 
+        //
     }
 
     /**
      * Register the package resources
-     *
-     * @return void
      */
-    private function registerResources(): void
+    private function bootResources(): void
     {
         //Load the views
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'belich');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'action-forms');
+    }
+
+    /**
+     * Register the package blade components
+     */
+    private function bootBladeComponents(): void
+    {
+        Blade::componentNamespace('Daguilarm\\ActionForms\\Components', 'action-forms');
+
+        $this->callAfterResolving(BladeCompiler::class, function (BladeCompiler $blade) {
+            $prefix = config('action-forms.prefix', '');
+
+            foreach (config('action-forms.components', []) as $alias => $component) {
+                $componentClass = is_string($component) ? $component : $component['class'];
+                $blade->component($componentClass, $alias, $prefix);
+            }
+        });
     }
 }
