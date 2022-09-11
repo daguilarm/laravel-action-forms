075930bafcbda447c030c25e99531da438c602ed
Damian-portatil <damian.aguilarm@gmail.com>
2022-09-11 22:52:20 +0200

    starting with select

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,75 @@
+@props([
+    'element' => $attributes->get('name'),
+    'uniqueKey' => $uniqueKey,
+    'css' => $css,
+    'conditional' => $conditional,
+    'width' => $width,
+    'label' => $label,
+    'dependOn' => $dependOn,
+    'dependOnType' => $dependOnType,
+    'dependOnValue' => $dependOnValue,
+    'helper' => $helper,
+    'addons' => $addons,
+    'after' => $after,
+    'before' => $before,
+    'value' => af__value($attributes, 'name', $data),
+])
+
+{{-- Form-element container --}}
+@if($viewAction !== 'show') 
+    <div 
+        x-data="formData(
+            '{{ $dependOn }}', 
+            @json($conditional ?? true), 
+            `{{ $value }}`, 
+            '{{ $dependOnValue }}', 
+            '{{ $dependOnType }}', 
+            databaseValue = null, 
+            $refs.__{{ $uniqueKey }}
+        )"
+        id="{{ $uniqueKey }}"
+        class="{{ $width }} {{ $cssElement }}"
+        x-show="visible"
+        :class="disabled ? disabledClass : ''"
+    >
+        {{-- Add label --}}
+        @includeWhen($label, 'action-forms::elements.label')
+        
+        {{-- Element container --}}
+        <div>
+            <div class="flex mt-1.5">
+                {{-- Addon before --}}
+                @if($before)
+                    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 {{ $css->get('addons') }} @error($element) {{ $css->get('errorHighlight') }} @else {{ $css->get('addonsHighlight') }} @enderror">
+                        {{ $before }}
+                    </span>
+                @endif
+                {{-- Select field --}}
+                {{-- <input 
+                    x-ref="__{{ $uniqueKey }}"
+                    x-on:change="enableOrDisableChildren($el)"
+                    :value="value"
+                    :disabled="disabled"
+                    data-key="{{ $uniqueKey }}"
+                    data-value="{{ $value }}"
+                    data-parent="{{ $dependOn }}"
+                    data-field="__{{ $uniqueKey }}"
+                    data-equal="{{ $dependOnValue }}"
+                    data-condition="{{ $conditional }}"
+                    dusk="form-input-{{ $attributes->get('id') ?? $element }}"
+                    class="{{ $css->get('base') }} {{ $addons }} @include('action-forms::elements.validation-highlight')" 
+                    {{-- Native attributes --}}
+                    {{ $attributes }} 
+                /> --}}
+                {{-- Addon after --}}
+                @if($after)
+                    <span class="af_element_disabled_{{ $uniqueKey }} inline-flex items-center px-3 rounded-r-md border border-l-0 {{ $css->get('addons') }} @error($element) {{ $css->get('errorHighlight') }} @else {{ $css->get('addonsHighlight') }} @enderror">
+                        {{ $after }}
+                    </span>
+                @endif
+            </div>
+            {{-- Validation errors and Helper --}}
+            @include('action-forms::elements.helper-and-validation')
+        </div>
+    </div> {{-- /Element container --}}
+@endif {{-- /Form element container --}}
---
+++ src/Components/Select.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Daguilarm\ActionForms\Components;
+
+use Daguilarm\ActionForms\Configable;
+use Daguilarm\ActionForms\FormComponent;
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
+
+class Select extends FormComponent
+{
+    use Configable;
+
+    public Collection $css;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public ?string $label = null,
+        public string $width = 'full',
+        public ?string $dependOn = null,
+        public string $dependOnType = 'disabled',
+        public mixed $dependOnValue = null,
+        public ?bool $conditional = null,
+        public ?string $helper = null,
+        public ?string $id = null,
+        public bool $asBoolean = false,
+        public string $position = 'vertical',
+        public array $options = []
+    ) {
+        parent::__construct();
+
+        $this->asBoolean = $asBoolean ? true : false;
+        $this->css = collect([
+            'base' => $this->getConfigClasses($this->getThemeCheckbox()),
+            'label' => $this->getConfigClasses($this->getThemeRadioLabel()),
+            'error' => $this->getConfigClasses($this->getThemeErrorMessages()),
+            'errorHighlight' => $this->getConfigClasses($this->getThemeErrorMessagesHighlight()),
+            'helper' => $this->getConfigClasses($this->getThemeHelper()),
+            'item' => $this->position === 'vertical'
+                ? config('action-forms.theme.radio.layout.vertical')
+                : config('action-forms.theme.radio.layout.horizontal'),
+        ]);
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View
+    {
+        return view('action-forms::components.radio');
+    }
+}
